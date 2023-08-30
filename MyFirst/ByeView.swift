1afbef76784684d5049bf2fd84f346e2a880afea
Vijaya Kosuri <vij.kosuri@gmail.com>
2023-08-30 19:15:02 -0400

    committing the first project

---
+++ MyFirst/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  MyFirst
+//
+//  Created by Srinivas Pala on 8/30/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
